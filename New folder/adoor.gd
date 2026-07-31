8e78d8b55e199ae5797dc76f5ed874dfb56b252c
Aashlesha Deshmukh <deshmukhaashlesha8@gmail.com>
2026-07-31 09:36:23 +0530

    add classes in main map and dood gd

---
+++ New folder/adoor.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+@export var next_scene: String = "res://scenes/maps/admin.tscn"
+
+var player_near = false
+
+func _ready():
+	print("script start")
+	
+func _on_body_entered(body):
+	print("Body entered:", body.name)
+	player_near = true	
+	
+func _on_body_exited(body):
+	print("Body exited:", body.name)
+	player_near = false
+	
+func _process(_delta):
+	if Input.is_action_just_pressed("interact"):
+		print("E pressed")
+		
+	if player_near and Input.is_action_just_pressed("interact"):	
+			print("Changing scene...")	
+			get_tree().change_scene_to_file(next_scene)
