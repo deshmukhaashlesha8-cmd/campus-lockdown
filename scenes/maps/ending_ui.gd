e8a1d7ed3c38949b73deccdedb26c006936e9a2d
Aashlesha Deshmukh <deshmukhaashlesha8@gmail.com>
2026-08-12 10:59:35 +0530

    level edone

---
+++ scenes/maps/ending_ui.gd
@@ -0,0 +1,29 @@
+extends CanvasLayer
+
+func show_ending():
+	visible = true
+
+	$background.modulate.a = 0
+	$popup.scale = Vector2(0.5, 0.5)
+	$popup.modulate.a = 0
+
+	var title = $popup/VBoxContainer/title
+	title.scale = Vector2(0.6, 0.6)
+
+	var title_tween = create_tween()
+	title_tween.tween_property(title, "scale", Vector2(1.15, 1.15), 0.2)
+	title_tween.tween_property(title, "scale", Vector2(1, 1), 0.15)
+
+	var t = create_tween()
+	t.tween_property($background, "modulate:a", 0.7, 0.3)
+	t.parallel().tween_property($popup, "scale", Vector2(1.05, 1.05), 0.25)
+	t.tween_property($popup, "scale", Vector2(1, 1), 0.15)
+	t.parallel().tween_property($popup, "modulate:a", 1.0, 0.3)
+
+	$confetti.emitting = true
+
+func _on_button_pressed() -> void:
+	get_tree().reload_current_scene()
+
+func _on_button_2_pressed() -> void:
+	get_tree().quit()
--- scenes/maps/level_up.gd
+++ scenes/maps/level_up.gd
@@ -10,11 +10,13 @@ func _process(delta):
 	if player_near and Input.is_action_just_pressed("interact"):
 		if GameManager.all_tasks_done():
 			ObjectiveManager.update_objective()
-			SceneTransition.fade_to_scene("res://ending.tscn")
-			
+
+			var ending = get_tree().current_scene.get_node("ending ui")
+			ending.show_ending()
+
 		else:
-			
 			PopupManager.show_message("Complete all department tasks first!")
+
 		player_near = false
 		await get_tree().create_timer(0.5).timeout
 		player_near = true
